699431d980aa311ca820750826a0b612dd802493
lampnthe173556 <lampnthe173556@fpt.edu.vn>
2024-09-23 23:14:49 +0700

    update code

--- src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
+++ src/main/java/vn/hoidanit/laptopshop/LaptopshopApplication.java
@@ -8,10 +8,7 @@ import org.springframework.context.ApplicationContext;
 public class LaptopshopApplication {
 
     public static void main(String[] args) {
-       ApplicationContext hoidanit =  SpringApplication.run(LaptopshopApplication.class, args);
-       for (String s : hoidanit.getBeanDefinitionNames()){
-           System.out.println(s);
-       }
+        SpringApplication.run(LaptopshopApplication.class, args);
     }
 
 }
